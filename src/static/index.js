e526abb127258ba67de6d8f8794cb5448550e9f1
CycIes <79021718+cycIes@users.noreply.github.com>
2025-11-24 00:44:28 -0500

    restrict key inputs

--- src/static/index.js
+++ src/static/index.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     let input = document.querySelector('#input');
     let fields = input.querySelectorAll('input');
-    let not_forward_keys = ['Backspace', 'Shift', 'ArrowLeft', 'ArrowRight', 'Tab']
+    let valid_special_keys = ['Backspace', 'Shift', 'ArrowLeft', 'ArrowRight', 'Tab'];
+    let valid_digit_keys = ['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
     fields.forEach((field) => field.addEventListener('keyup', (e) => {
-        if (!(not_forward_keys.includes(e.key))) 
+        if (valid_digit_keys.includes(e.key))
         {
             field.blur();
             next = field.nextElementSibling;
@@ -15,14 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }));
     fields.forEach((field) => field.addEventListener('keydown', (e) => {
-        if ((e.key === 'Backspace' && field.value === '') || e.key === 'ArrowLeft')
+        key = e.key;
+        if ((key === 'Backspace' && field.value === '') || key === 'ArrowLeft')
         {
-            field.blur()
-            previous = field.previousElementSibling
+            field.blur();
+            previous = field.previousElementSibling;
             if (previous)
             {
                 previous.focus();
             }
+        }
+        else if (!((valid_digit_keys.includes(key)) || (valid_special_keys).includes(key)))
+        {
+            e.preventDefault();
         }
     }));
 });
