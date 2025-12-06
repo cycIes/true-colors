550462c5855f24346e67e7e61eb13be7ece4e381
CycIes <79021718+cycIes@users.noreply.github.com>
2025-12-05 19:37:38 -0500

    create keyboard in ui and add some ui improvements

--- src/app.py
+++ src/app.py
@@ -11,36 +11,39 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
-# color
-# attempts
-# count
-# won
-
 def random_color():
     color = ""
     for i in range(3):
-        color += str(hex(randrange(0, 255)))[2:].zfill(2)
+        color += str(hex(randrange(0, 255)))[2:].zfill(2).upper()
     return color
+
+def appendUnique(element, list):
+    if element not in list:
+        list.append(element)
 
 def reset():
     global color
     global attempts
     global count
     global won
+    global correctDigits
+    global partialDigits
+    global incorrectDigits
     
     color = random_color()
-    # color = "d46dd6"
     attempts = []
     count = 0
     won = False
+    correctDigits = []
+    partialDigits = []
+    incorrectDigits = []
 
 reset()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", color=f"#{color}", hex=color, attempts=attempts, victory=False, count=count)
+    return render_template("index.html", color=f"#{color}", hex=color, attempts=attempts, victory=False)
 
 @app.route("/check", methods=["POST"])
 def check():
@@ -52,14 +55,15 @@ def check():
     guessed_color = "#"
     length = len(color)
     
-    digits = [input for input in answers]
+    digits = [input.upper() for input in answers]
     remaining = [digit for digit in color]
     partial_guesses = []
     for i in range(length):
-        digit = digits[i].lower()
+        digit = digits[i]
         if digit == color[i]:
             correct = "correct"
             remaining[i] = ""
+            appendUnique(digit, correctDigits)
         elif digit in color:
             partial_guesses.append({"digit": digit, "index": i})
             correct = "incorrect"
@@ -67,8 +71,9 @@ def check():
         else:
             correct = "incorrect"
             victory = False
+            appendUnique(digit, incorrectDigits)
         attempt.append({"value": digit, "correct": correct})
-        guessed_color += digits[i]
+        guessed_color += digit
     
     print(partial_guesses)
     print(remaining)
@@ -77,6 +82,7 @@ def check():
             index = remaining.index(guess["digit"])
             attempt[guess["index"]]["correct"] = "partial"
             remaining[index] = ""
+            appendUnique(digit, partialDigits)
             
     attempts.append({"attempt": attempt, "color": guessed_color})
     
--- src/static/index.js
+++ src/static/index.js
@@ -1,34 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let input = document.querySelector('#input');
-    let fields = input.querySelectorAll('input');
-    let valid_special_keys = ['Backspace', 'Shift', 'ArrowLeft', 'ArrowRight', 'Tab'];
-    let valid_digit_keys = ['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const input = document.querySelector('#input');
+    const fields = input.querySelectorAll('input');
+    const valid_special_keys = ['Backspace', 'Shift', 'ArrowLeft', 'ArrowRight', 'Tab'];
+    const valid_digit_keys = ['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const keyboard = document.querySelector('#keyboard');
+    const key_btns = document.querySelectorAll('.key');
 
+    function focusNext(current)
+    {
+        const next = current.nextElementSibling;
+        if (next)
+        {
+            next.focus();
+        }
+    }
+
+    function focusPrevious(current)
+    {
+        const previous = current.previousElementSibling;
+        if (previous)
+        {
+            previous.focus();
+        }
+    }
+
+    // Focus next input element after typing a valid key
     fields.forEach((field) => field.addEventListener('keyup', (e) => {
-        if (valid_digit_keys.includes(e.key))
+        if (valid_digit_keys.includes(e.key) || e.key === 'ArrowRight')
         {
             field.blur();
-            next = field.nextElementSibling;
-            if (next) 
-            {
-                next.focus();
-            }
+            focusNext(field);
         }
     }));
+
+    // Prevent typing invalid keys, add special key behavior, and focus previous input element on special keys
     fields.forEach((field) => field.addEventListener('keydown', (e) => {
-        key = e.key;
+        const key = e.key;
         if ((key === 'Backspace' && field.value === '') || key === 'ArrowLeft')
         {
             field.blur();
-            previous = field.previousElementSibling;
-            if (previous)
-            {
-                previous.focus();
-            }
+            focusPrevious(field);
         }
         else if (!((valid_digit_keys.includes(key)) || (valid_special_keys).includes(key)))
         {
             e.preventDefault();
         }
+        else if (valid_digit_keys.includes(key))
+        {
+            field.value = key.toUpperCase();
+            e.preventDefault();
+        }
+        else if (key === 'Backspace')
+        {
+            field.value = '';
+        }
     }));
+
+    // Type with provided keyboard
+    keyboard.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.classList.contains('key') || target.parentElement.classList.contains('key')) 
+        {
+            const focused = document.activeElement;
+            if (Array.from(fields).includes(focused))
+            {
+                if (target.id === 'backspace' || target.parentElement.id === 'backspace')
+                {
+                    if (focused.value === '')
+                    {
+                        focusPrevious(focused);
+                        return;
+                    }
+                    focused.value = '';
+                    return;
+                }
+                focused.value = target.id;
+                focusNext(focused);
+            }
+        }
+    });
+    
+    // Prevent input fields from losing focus on button click
+    keyboard.addEventListener('focusin', (e) => {
+        const related = e.relatedTarget;
+        if (related && Array.from(fields).includes(related)) {
+            related.focus();
+        }
+    })
 });
